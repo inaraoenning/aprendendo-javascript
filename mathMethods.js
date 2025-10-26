7783379cfd768704176ce1080b28c1da44d8bcdc
Feyre_head <inaraoenning@gmail.com>
2025-10-26 14:53:43 -0300

    Add Math Methods

---
+++ mathMethods.js
@@ -0,0 +1,15 @@
+//Metodos Matematicos (Math)
+
+let num1 = 2.5;
+
+console.log(Math.round(num1)) //Arredondamento de acordo com a regra 
+console.log(Math.ceil(num1))  //Arredondamento para cima independente do numero
+console.log(Math.floor(num1)) //Arredondamento para baixo idependente do numero
+console.log(Math.sqrt(num1))  //Raiz Quadrada
+console.log(Math.pow(num1, 3))  //(num, elevado a potência)
+console.log(Math.abs(num1))  //Valor absoluto
+console.log(Math.Random())  //Valor Random
+console.log(Math.Random() * 10 + 1)  // Valor Random do 1 ao 10
+console.log(Math.round (Math.Random() * 10 + 1))  // Gera num de 1 a 10 com num arredondado
+
+
