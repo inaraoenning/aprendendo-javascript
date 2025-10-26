ae6731650cb72178ac887a65ff7f18711c4c8ce5
Feyre_head <inaraoenning@gmail.com>
2025-10-26 15:08:09 -0300

    Metodos Date()

---
+++ dateMethods.js
@@ -0,0 +1,16 @@
+// Data e Hora (Métodos)
+
+let data = new Date()
+
+// O Dia do Mês
+console.log(data.getDate())
+
+// O Dia da Semana (0-6) onde 0 é Domingo
+console.log(data.getDay)
+
+// O Mês (0-11) onde 0 é Janeiro
+console.log(data.getMonth())
+
+// O Ano
+console.log(data.getFullYear)
+
